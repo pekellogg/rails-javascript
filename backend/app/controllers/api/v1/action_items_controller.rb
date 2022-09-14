f00c2b4db2cb942f2350049865c9dcfd590ac0ef
Peyton Kellogg <pekellogg@hotmail.com>
2022-09-14 02:40:56 -0700

    bulk of MVP controllers

--- backend/app/controllers/api/v1/action_items_controller.rb
+++ backend/app/controllers/api/v1/action_items_controller.rb
@@ -1,25 +1,39 @@
-class ActionItemsController < ApplicationController
+class Api::V1::ActionItemsController < ApplicationController
   wrap_parameters ActionItem # https://api.rubyonrails.org/v7.0.4/classes/ActionController/ParamsWrapper.html
   
   def index
+    @action_items = ActionItem.all
+    render @action_items
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
+    @action_item = ActionItem.create(action_item_params)
+    render @action_item if @action_item.save
   end
 
   def show
+    @action_item = ActionItem.find(params[:id])
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def update
+    @action_item = ActionItem.find(params[:id])
+    render @action_item if @action_item.update(action_item_params)
   end
 
-  def destory
+  def destroy
+    ActionItem.find(params[:id]).destroy
+  end
+
+  private
+
+  def action_item_params
+    params.require(:action_item).permit(:id, :assignees, :status, :deadline, :created_at, :updated_at)
   end
   
 end
--- backend/app/controllers/api/v1/action_plans_controller.rb
+++ backend/app/controllers/api/v1/action_plans_controller.rb
@@ -1,25 +1,38 @@
-class ActionPlansController < ApplicationController
+class Api::V1::ActionPlansController < ApplicationController
   wrap_parameters ActionPlan # https://api.rubyonrails.org/v7.0.4/classes/ActionController/ParamsWrapper.html
   
   def index
+    @action_plans = ActionPlan.all
+    render @action_plans
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
+    @action_plan = ActionPlan.create(action_plan_params)
+    render @action_plan if @action_plan.save
   end
 
   def show
+    @action_plan = ActionPlan.find(params[:id])
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def update
+    @action_plan = ActionPlan.find(params[:id])
   end
 
-  def destory
+  def destroy
+    ActionPlan.find(params[:id]).destroy
+  end
+
+  private
+
+  def action_plan_params
+    params.require(:action_plan).permit(:id, :creator, :collaborators, :status, :goal, :deadline, :deadline_reason, :category, :percent_complete, :created_at, :updated_at, action_item_ids: [])
   end
   
 end
